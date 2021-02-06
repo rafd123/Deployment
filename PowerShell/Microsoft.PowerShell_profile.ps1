733920d34de251f90820ca04bf8194760ebf108b
Rafael Dowling Goodman <rafael@rocketwing.io>
2021-02-06 14:32:31 -0500

    Import chocolatey

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,9 @@
 ﻿# Initialize-PowerLinePrompt
 # Set-Theme RafD123
 Set-PoshPrompt -Theme ~\OneDrive\Documents\PowerShell\PoshThemes\RafD123.omp.json
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
