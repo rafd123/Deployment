be15c7efadb58aefc884cb89b09491a9496de6bd
Rafael Dowling Goodman <rafael@rocketwing.io>
2021-02-07 15:53:12 -0500

    Only enable oh-my-posh prompt for color terminals

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 ﻿# Initialize-PowerLinePrompt
 # Set-Theme RafD123
-Set-PoshPrompt -Theme ~\OneDrive\Documents\PowerShell\PoshThemes\RafD123.omp.json
+
+if (-not $env:SSH_CONNECTION -or $env:TERM -like '*color*') {
+    Set-PoshPrompt -Theme ~\OneDrive\Documents\PowerShell\PoshThemes\RafD123.omp.json
+}
 
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
